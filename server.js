9b23f2fc2899d701a8403a086ce36ba6294d17ac
Caleb Pigues <cap@macattack.local>
2023-04-10 00:01:15 -0400

    Picture upload complete

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const { spawn } = require('child_process');
 const express = require("express")
 const cors = require('cors')
+const fileUpload = require('express-fileupload');
+const fs = require('fs')
+var base64Img = require('base64-img');
 const bodyParser = require('body-parser');
 const app = express()
 
 require("dotenv").config()
 const PORT = process.env.PORT || 3000
 
-app.use(cors({origin: ['http://localhost:19008']}))
+app.use(cors({origin: ['http://localhost:19006', '*']}))
 app.use(bodyParser.json());
+app.use(fileUpload({
+    tempFileDir: './uploads/',
+    useTempFiles: true,
+}));
 
 app.get('/classes', (req, res) => {
     res.json({
@@ -20,13 +27,28 @@ app.get('/classes', (req, res) => {
     })
 })
 
+app.post('/upload', (req, res) => {
+    // Access the uploaded file using req.files
+    const uploadedFile = req.files.file;
+    console.log(uploadedFile.values)
+    res.send(uploadedFile)
+});
+ 
 app.post('/predict', (req, res) => {
-    const { files } = req.body
-    let pred_data = {}
+    let file;
+    if (req.body) {
+        file = base64Img.imgSync(req.body.file, 'uploads', 'file')
+    }
 
-    const pythonProcess = spawn('python', ['./pyscripts/models.py', JSON.stringify(files)]);
+    if (req.files && req.files.file) {
+        file = req.files.file.tempFilePath
+    }
+
+    let pred_data = {}
+    const pythonProcess = spawn('python', ['./pyscripts/models.py', JSON.stringify([file])]);
     pythonProcess.stdout.on('data', (data) => {
         try {
+            console.log(JSON.parse(data.toString()))
             pred_data = JSON.parse(data.toString())
         } catch (error) {
             console.log(data.toString())
@@ -41,6 +63,7 @@ app.post('/predict', (req, res) => {
     pythonProcess.on('exit', (code) => {
         console.log(`Python script exited with code ${code}`);
         if (code == 0) {
+            console.log(pred_data)
             res.status(200).json(pred_data)
         } else {
             res.status(400).json({message: "Python script error", error: pred_data['stderr']})
